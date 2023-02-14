37a82e057cfc4a09b1acddbe95fdbe207c04fb59
jeimy555 <yuranyyurbleidys555@gmail.com>
2023-02-14 11:04:30 -0500

    identificador de numeros

--- src/Discotec.java
+++ src/Discotec.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Age {
+public class Discotec{
     public static void main(String[] args) {
         Scanner lectura= new Scanner(System.in) ;
         int age;
---
+++ src/Number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Number {
+    public static void main(String[] args) {
+        Scanner lectura= new Scanner(System.in);
+
+int number;
+System.out.println("ingrese un numero");
+number=lectura.nextInt();
+if (number>0) {System.out.println("el numero es positivo");
+    
+}else if (number==0) {System.out.println("el numero es neutro");
+    
+}else{
+    System.out.println("el numero es negativo");
+}
+
+
+    }
+    
+}
